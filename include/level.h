152b53632aa311e7d34dc586cf25fab68c41e4bb
unknown <iophamza2@gmail.com>
2022-04-29 20:33:12 +0000

    load level/ save level/ setTileset

--- include/level.h
+++ include/level.h
@@ -21,16 +21,17 @@ class Level
     std::fstream level_file;
     Texture tileset;
 
-    inline bool parse_level();
+    inline void parse_level();
 
     public:    
     
-    std::vector<std::vector<int>> matrix;
+    std::vector<std::vector<int>> matrix[5];
 
     Level(std::string level_path,std::string tileset_path);
     void LoadLevel(std::string path = "");
+    void SaveLevel(std::string path);
     void DrawLevel();
-    void setTileSet(Texture texture);
+    void setTileSet(std::string path);
     
 };
 
@@ -39,9 +40,13 @@ Level::Level(std::string level_path,std::string tileset_path){
     this->tileset_path = tileset_path;
 }
 
-bool Level::parse_level(){
+void Level::parse_level(){
     std::string token;
     bool debug = true;
+
+    level_file >> token;
+    if(debug) std::cout<<token<<'\n';
+    tileset_path = token;
 
     level_file >> token;
     if(debug) std::cout<<token<<'\n';
@@ -71,7 +76,21 @@ bool Level::parse_level(){
     if(debug) std::cout<<token<<'\n';
     player_pos.y = stoi(token);
 
-    
+    int n = width/tile_width, m = height/tile_height;
+
+    for(int i = 0;i<5;i++)matrix[i].clear();
+
+    for(int k = 0;k<nbr_layers;k++){
+        matrix[k].resize(m);
+        for(int j = 0;j<m;j++){
+            matrix[k][j].resize(n);
+            for(int i = 0;i<n;i++){
+                level_file >> token;
+                matrix[k][j][i] = stoi(token);
+            }
+        }
+    }
+
 }
 
 void Level::LoadLevel(std::string path=""){
@@ -85,14 +104,43 @@ void Level::LoadLevel(std::string path=""){
         return;
     }
 
-    if(!parse_level()){
-        std::cout<<"unable to load level, damaged file\n";
-    }
-
+    parse_level();
+    //std::cout<<"unable to load level, damaged file\n";
     
-
+    level_file.close();
 }
 
+void Level::SaveLevel(std::string path){
+    level_file.open(path,std::ios::out | std::ios::trunc);
+
+    level_file << width<<'\n';
+    level_file <<height<<'\n';
+    level_file <<tile_width<<'\n';
+    level_file <<tile_height<<'\n';
+    level_file <<nbr_layers<<'\n';
+    level_file <<player_pos.x<<'\n';
+    level_file <<player_pos.y<<'\n';
+
+    int n = width/tile_width, m = height/tile_height;
+
+    for(int k = 0;k<nbr_layers;k++){
+        for(int j = 0;j<m;j++){
+            for(int i = 0;i<n;i++){
+                level_file << matrix[k][j][i];
+            }
+        }
+    }
+
+    level_file.close();
+}
+
+void Level::setTileSet(std::string path){
+    tileset = LoadTexture(path.c_str());
+}
+
+void Level::DrawLevel(){
+    
+}
 
 
 #endif

609b206716f74c5fb8be4b854939d86392104389
unknown <iophamza2@gmail.com>
2022-04-30 02:31:08 +0000

    move tileset / zoom tileset and level / nbr layer level == 0 /

--- include/level.h
+++ include/level.h
@@ -8,13 +8,12 @@
 
 class Level
 {
-    private:
-
+private:
     std::string tileset_path;
     std::string level_path;
 
-    int width,height;
-    int tile_width,tile_height;
+    int width, height;
+    int tile_size;
     int nbr_layers;
     Vector2 player_pos;
 
@@ -23,109 +22,125 @@ class Level
 
     inline void parse_level();
 
-    public:    
-    
+public:
     std::vector<std::vector<int>> matrix[5];
 
-    Level(std::string level_path,std::string tileset_path);
+    Level(std::string level_path, std::string tileset_path);
     void LoadLevel(std::string path = "");
     void SaveLevel(std::string path);
     void DrawLevel();
     void setTileSet(std::string path);
-    
 };
 
-Level::Level(std::string level_path,std::string tileset_path){
+Level::Level(std::string level_path, std::string tileset_path)
+{
     this->level_path = level_path;
     this->tileset_path = tileset_path;
 }
 
-void Level::parse_level(){
+void Level::parse_level()
+{
     std::string token;
     bool debug = true;
 
     level_file >> token;
-    if(debug) std::cout<<token<<'\n';
+    if (debug)
+        std::cout << token << '\n';
     tileset_path = token;
 
     level_file >> token;
-    if(debug) std::cout<<token<<'\n';
+    if (debug)
+        std::cout << token << '\n';
     width = stoi(token);
 
     level_file >> token;
-    if(debug) std::cout<<token<<'\n';
+    if (debug)
+        std::cout << token << '\n';
     height = stoi(token);
 
     level_file >> token;
-    if(debug) std::cout<<token<<'\n';
-    tile_width = stoi(token);
+    if (debug)
+        std::cout << token << '\n';
+    tile_size = stoi(token);
 
     level_file >> token;
-    if(debug) std::cout<<token<<'\n';
-    tile_height = stoi(token);
-
-    level_file >> token;
-    if(debug) std::cout<<token<<'\n';
+    if (debug)
+        std::cout << token << '\n';
     nbr_layers = stoi(token);
 
-    level_file>>token;
-    if(debug) std::cout<<token<<'\n';
+    level_file >> token;
+    if (debug)
+        std::cout << token << '\n';
     player_pos.x = stoi(token);
-    
-    level_file>>token;
-    if(debug) std::cout<<token<<'\n';
+
+    level_file >> token;
+    if (debug)
+        std::cout << token << '\n';
     player_pos.y = stoi(token);
 
-    int n = width/tile_width, m = height/tile_height;
+    int n = width / tile_size, m = height / tile_size;
 
-    for(int i = 0;i<5;i++)matrix[i].clear();
+    for (int i = 0; i < 5; i++)
+        matrix[i].clear();
 
-    for(int k = 0;k<nbr_layers;k++){
+
+    for (int k = 0; k < 5; k++)
+    {
         matrix[k].resize(m);
-        for(int j = 0;j<m;j++){
+        for (int j = 0; j < m; j++)
+        {
             matrix[k][j].resize(n);
-            for(int i = 0;i<n;i++){
+            for (int i = 0; i < n; i++)
+            {
                 level_file >> token;
-                matrix[k][j][i] = stoi(token);
+                if(nbr_layers == 0)
+                    matrix[k][j][i] = -1;
+                else
+                    matrix[k][j][i] = stoi(token);
             }
         }
     }
-
 }
 
-void Level::LoadLevel(std::string path=""){
-    if(path.length()>0){
+void Level::LoadLevel(std::string path = "")
+{
+    if (path.length() > 0)
+    {
         this->level_path = path;
     }
 
-    level_file.open(this->level_path,std::ios::in);
-    if(!level_file.is_open()){
-        std::cout<<"error : can't open file \""<<this->level_path<<"\"\n";
+    level_file.open(this->level_path, std::ios::in);
+    if (!level_file.is_open())
+    {
+        std::cout << "error : can't open file \"" << this->level_path << "\"\n";
         return;
     }
 
     parse_level();
-    //std::cout<<"unable to load level, damaged file\n";
-    
+    // std::cout<<"unable to load level, damaged file\n";
+
     level_file.close();
 }
 
-void Level::SaveLevel(std::string path){
-    level_file.open(path,std::ios::out | std::ios::trunc);
+void Level::SaveLevel(std::string path)
+{
+    level_file.open(path, std::ios::out | std::ios::trunc);
 
-    level_file << width<<'\n';
-    level_file <<height<<'\n';
-    level_file <<tile_width<<'\n';
-    level_file <<tile_height<<'\n';
-    level_file <<nbr_layers<<'\n';
-    level_file <<player_pos.x<<'\n';
-    level_file <<player_pos.y<<'\n';
+    level_file << width << '\n';
+    level_file << height << '\n';
+    level_file << tile_size << '\n';
+    level_file << nbr_layers << '\n';
+    level_file << player_pos.x << '\n';
+    level_file << player_pos.y << '\n';
 
-    int n = width/tile_width, m = height/tile_height;
+    int n = width / tile_size, m = height / tile_size;
 
-    for(int k = 0;k<nbr_layers;k++){
-        for(int j = 0;j<m;j++){
-            for(int i = 0;i<n;i++){
+    for (int k = 0; k < nbr_layers; k++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            for (int i = 0; i < n; i++)
+            {
                 level_file << matrix[k][j][i];
             }
         }
@@ -134,13 +149,28 @@ void Level::SaveLevel(std::string path){
     level_file.close();
 }
 
-void Level::setTileSet(std::string path){
+void Level::setTileSet(std::string path)
+{
     tileset = LoadTexture(path.c_str());
 }
 
-void Level::DrawLevel(){
-    
+void Level::DrawLevel()
+{
+    int n = width / tile_size, m = height / tile_size;
+    int n_row_tiles = tileset.width / tile_size;
+    int ti, tj;
+    for (int k = 0; k < nbr_layers; k++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                tj = matrix[k][j][i] / n_row_tiles;
+                ti = matrix[k][j][i] % n_row_tiles;
+                DrawTextureRec(tileset, (Rectangle){ti * n_row_tiles, tj * n_row_tiles, tile_size, tile_size}, (Vector2){320 + i * tile_size, j * tile_size}, WHITE);
+            }
+        }
+    }
 }
-
 
 #endif
--- main.cpp
+++ main.cpp
@@ -12,13 +12,14 @@
 #define S_WIDTH 1080
 #define S_HEIGHT 720
 
+
 int main(){
     InitWindow(S_WIDTH,S_HEIGHT,"PP : Level Editor");    
     SetTargetFPS(60);
     GuiEnable();  
 
     std::vector<Rectangle> gui_rec;
-    int focused = 0;
+    int focused = -1;
 
     gui_rec.push_back((Rectangle){10,10,100,40});int textbox_tileset = 0; //textbox to load tileset
     gui_rec.push_back((Rectangle){120,10,70,40});int button_tileset = 1; //btn to load the tileset
@@ -27,19 +28,37 @@ int main(){
     gui_rec.push_back((Rectangle){200,10,100,40});int textbox_level = 2; //textbox to load level
     gui_rec.push_back((Rectangle){310,10,70,40});int button_level = 3; //btn to load level
 
+    gui_rec.push_back((Rectangle){500,10,100,40});int spinner_tileset = 4;
+    gui_rec.push_back((Rectangle){700,10,100,40});int spinner_level = 5;
+
     char* Tileset_path = new char[100];
     strcpy(Tileset_path,"Tileset path");
     char* Level_path = new char[100];
     strcpy(Level_path,"Level path");
-    Texture TileSet;
+    Texture TileSet = LoadTexture("tileset.png");
 
     std::fstream level;
 
+    Rectangle TileSet_view = (Rectangle){10,60,350,600};
+
+    Vector2 level_pos = (Vector2){0};
+    int level_scale=1;
+
+    Vector2 tileset_pos = (Vector2){TileSet_view.x,TileSet_view.y};
+    int tileset_scale=1;
+    
     while (!WindowShouldClose())
     {
+
+        if(IsKeyDown(KEY_A))tileset_pos.x-=6;
+        if(IsKeyDown(KEY_D))tileset_pos.x+=6;
+        if(IsKeyDown(KEY_S))tileset_pos.y+=6;
+        if(IsKeyDown(KEY_W))tileset_pos.y-=6;
+
+
         BeginDrawing();
 
-        ClearBackground(DARKGRAY);
+        ClearBackground((Color){33,33,33,1});
 
         //focus on the clicked gui rectangle
         for(int i = 0;i<gui_rec.size();i++){
@@ -48,20 +67,26 @@ int main(){
                 break;
             }
         }
-
         
-        GuiTextBox(gui_rec[textbox_tileset],Tileset_path,100,focused==0);
+        GuiTextBox(gui_rec[textbox_tileset],Tileset_path,100,focused==textbox_tileset);
         if(GuiButton(gui_rec[button_tileset],"Load")){
             TileSet = LoadTexture(Tileset_path);
         }
 
-        GuiTextBox(gui_rec[textbox_level],Level_path,100,focused==0);
+        GuiTextBox(gui_rec[textbox_level],Level_path,100,focused==textbox_level);
         if(GuiButton(gui_rec[button_level],"Load")){
             TileSet = LoadTexture(Level_path);
         }
 
-        DrawTexture(TileSet,10,60,WHITE);
+        GuiSpinner(gui_rec[spinner_tileset],"zoom tileset",&tileset_scale,1,4,focused==spinner_tileset);
+        GuiSpinner(gui_rec[spinner_level],"zoom level",&level_scale,1,4,focused==spinner_level);
 
+        BeginScissorMode(TileSet_view.x,TileSet_view.y,TileSet_view.width,TileSet_view.height);
+
+        ClearBackground(LIGHTGRAY);
+        DrawTextureEx(TileSet,tileset_pos,0,tileset_scale,WHITE);
+
+        EndScissorMode();
 
         EndDrawing();
     }
